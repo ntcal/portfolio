e7c7e8251a94f1ebb6e4b8bed2c06973e45b1233
ntcal <Izunduokpokontc@gmail.com>
2024-09-27 20:00:19 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+const person = {
+    firstName:"John",
+    lastName: "Doe",
+    fullName: function () {
+      return this.firstName + " " + this.lastName;
+    }
+  }
+  
+  // This will return "John Doe":
+  person.fullName(John Doe );  
+
+  if (hour < 14) {
+    greeting = "welcome";
+  } else {
+    greeting = "bye";
+  }
